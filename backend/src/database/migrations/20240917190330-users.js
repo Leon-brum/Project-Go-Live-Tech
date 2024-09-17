f1492dcd8df942f45b9894192a60a8e8097b6859
Leonardo Moreno <leu-profissional@hotmail.com>
2024-09-17 16:14:47 -0300

    configuracoes iniciais

---
+++ backend/src/database/migrations/20240917190330-users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      user: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
